d8464178fab00ffd995e650f395804171c37c7cd
akong <noobakong@163.com>
2023-04-27 02:28:49 +0800

    chore: test auto develop

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Knowledge Nexus',
-  tagline: 'Akong Test',
+  tagline: 'Akong Test oo',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
